d7034177e4cd7c725c6cdfabe81220c8ce931c3f
DenisBritoUnicv <97901679+DenisBritoUnicv@users.noreply.github.com>
2022-01-21 15:22:08 -0100

    LoginVersao1

--- src/App.js
+++ src/App.js
@@ -3,22 +3,32 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+ <div>
+   <body>
+   <div class="main">  	
+		<input type="checkbox" id="chk" aria-hidden="true"/>
+
+			<div class="signup">
+				<form>
+					<label for="chk" aria-hidden="true">Sign up</label>
+					<input type="text" name="txt" placeholder="User name" required=""/>
+					<input type="email" name="email" placeholder="Email" required=""/>
+					<input type="password" name="pswd" placeholder="Password" required=""/>
+					<button>Sign up</button>
+				</form>
+			</div>
+
+			<div class="login">
+				<form>
+					<label for="chk" aria-hidden="true">Login</label>
+					<input type="email" name="email" placeholder="Email" required=""/>
+					<input type="password" name="pswd" placeholder="Password" required=""/>
+					<button>Login</button>
+				</form>
+			</div>
+	</div>
+</body>
+ </div>
   );
 }
 
